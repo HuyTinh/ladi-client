6eeab7e3e4bc39029e3219d36ff264e9fc4b5373
Huy Tinh <170064219+HuyTinh@users.noreply.github.com>
2024-10-13 02:45:08 +0700

    update new version ladi-server@0.0.4

--- js/ladi-server@0.0.4.js
+++ js/ladi-server@0.0.4.js
@@ -3,10 +3,16 @@ const headTags = [
         tagName: "link",
         attributes:
         {
-            href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-            rel: "stylesheet",
-            integrity: "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN",
-            crossOrigin: "anonymous"
+            href: "https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css",
+            rel: "stylesheet"
+        }
+
+    }, {
+        tagName: "link",
+        attributes:
+        {
+            href: "https://cdn.jsdelivr.net/npm/daisyui@4.12.13/dist/full.min.css",
+            rel: "stylesheet"
         }
 
     },
@@ -69,6 +75,8 @@ initBottomLinks();
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getDatabase, ref, child, get, set, update, remove } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
+
+
 
 
 // Your web app's Firebase configuration
